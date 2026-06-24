8363b3ede533b4b32d06dce5e226eb4cfe3eeae2
sirohiaryan <168159917+sirohiaryan@users.noreply.github.com>
2026-06-25 03:20:35 +0530

    Delete backend directory

--- backend/src/db.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import { env } from "./env";
-
-export async function connectDb() {
-  mongoose.set("strictQuery", true);
-  await mongoose.connect(env.MONGODB_URI);
-}
